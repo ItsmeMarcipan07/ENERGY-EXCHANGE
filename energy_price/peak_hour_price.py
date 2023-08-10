90c48340704fe1cd2abf77c04c723cf266c3053d
ItsmeMarcipan07 <martipan07@abv.bg>
2023-08-10 14:35:11 +0300

    Update peak_hour_price.py

--- energy_price/peak_hour_price.py
+++ energy_price/peak_hour_price.py
@@ -1,5 +1,6 @@
 import csv
 
+
 def peak_hour(path):
     with open(path, mode='r') as file:
         csvFile = csv.reader(file)
@@ -14,23 +15,23 @@ def peak_hour(path):
         for lines in csvFile:
             if lines[0] == '' or not 55 < int(lines[0]) < 140:
                 continue
-
-            if row == 1:
-                first_day += float(lines[5])
-            elif row == 2:
-                second_day += float(lines[5])
-            elif row == 3:
-                third_day += float(lines[5])
-            elif row == 4:
-                fourth_day += float(lines[5])
-            elif row == 5:
-                fifth_day += float(lines[5])
-            elif row == 6:
-                sixth_day += float(lines[5])
-            elif row == 7:
-                seventh_day += float(lines[5])
-                row = 1
-                continue
+            match row:
+                case 1:
+                    first_day += float(lines[5])
+                case 2:
+                    second_day += float(lines[5])
+                case 3:
+                    third_day += float(lines[5])
+                case 4:
+                    fourth_day += float(lines[5])
+                case 5:
+                    fifth_day += float(lines[5])
+                case 6:
+                    sixth_day += float(lines[5])
+                case 7:
+                    seventh_day += float(lines[5])
+                    row = 1
+                    continue
             row += 1
-        return [first_day / 12, second_day / 12, third_day / 12, fourth_day / 12, fifth_day / 12, sixth_day / 12,
-                seventh_day / 12]
+        return [f'{first_day / 12:.2f}', f'{second_day / 12:.2f}', f'{third_day / 12:.2f}', f'{fourth_day / 12:.2f}',
+                f'{fifth_day / 12:.2f}', f'{sixth_day / 12:.2f}', f'{seventh_day / 12:.2f}']
